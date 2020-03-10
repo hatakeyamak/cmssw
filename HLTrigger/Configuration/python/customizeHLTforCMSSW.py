2b29b4302826f66bbfca38d0fe88ac087494531d
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2020-03-10 16:26:43 +0100

    Drop PS from PFBlockAlgo for HLT

--- HLTrigger/Configuration/python/customizeHLTforCMSSW.py
+++ HLTrigger/Configuration/python/customizeHLTforCMSSW.py
@@ -164,10 +164,30 @@ def customiseFor2017DtUnpacking(process):
 
     return process
 
+def customiseForPFBlockAlgoPSDrop(process):
+
+    listHltPFBlock=['hltParticleFlowBlock',
+                    'hltParticleFlowBlockForTaus',
+                    'hltParticleFlowBlockReg']
+    for att in listHltPFBlock:
+        if hasattr(process,att):
+            prod = getattr(process, att)
+            vpset_linkdef = prod.linkDefinitions
+            for pset in reversed(vpset_linkdef):
+                if pset.linkType == 'PS1:ECAL' or pset.linkType == 'PS2:ECAL':
+                    vpset_linkdef.remove(pset)
+            vpset_elemimport = prod.elementImporters
+            for pset in reversed(vpset_elemimport):
+                if pset.source == cms.InputTag( "hltParticleFlowClusterPSUnseeded" ):
+                    vpset_elemimport.remove(pset)
+
+    return process
+
 # CMSSW version specific customizations
 def customizeHLTforCMSSW(process, menuType="GRun"):
 
     # add call to action function in proper order: newest last!
     # process = customiseFor12718(process)
+    process = customiseForPFBlockAlgoPSDrop(process)
 
     return process
