384b48860b0cb4ac3bda6a3b30751e97a7975d7d
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2022-10-05 15:50:27 -0500

    fix typo.

--- RecoParticleFlow/PFClusterProducer/python/particleFlowClusterHBHE_cfi.py
+++ RecoParticleFlow/PFClusterProducer/python/particleFlowClusterHBHE_cfi.py
@@ -133,7 +133,7 @@ from Configuration.ProcessModifiers.run2_HECollapse_2018_cff import run2_HEColla
         positionCalc = dict(logWeightDenominatorByDetector = {1 : dict(logWeightDenominator = _thresholdsHEphase1) } ),
         allCellsPositionCalc = dict(logWeightDenominatorByDetector = {1 : dict(logWeightDenominator = _thresholdsHEphase1) } ),
     ),
-                                                7)
+)
 (run2_HE_2018 & ~run2_HECollapse_2018).toModify(_particleFlowClusterHBHE_cuda,
     seedFinder = dict(thresholdsByDetector = {1 : dict(seedingThreshold = _seedingThresholdsHEphase1) } ),
     initialClusteringStep = dict(thresholdsByDetector = {1 : dict(gatheringThreshold = _thresholdsHEphase1) } ),
